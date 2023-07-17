cc7902d3c07beae36ab6366d9b236e642011d89f
salamanderctesiphon <swbrookshire@gmail.com>
2023-07-17 14:17:32 -0500

    created a server

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/') {
+/*   if(req.url === '/') {
     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
       if(err) throw err;
-      res.writeHead(200, { 'Contnet': 'text/html'});
+      res.writeHead(200, { 'Content': 'text/html'});
       res.end(content);
     }) 
   }
   if(req.url === '/about') {
     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
       if(err) throw err;
-      res.writeHead(200, { 'Contnet': 'text/html'});
+      res.writeHead(200, { 'Content': 'text/html'});
       res.end(content);
-    }) 
-  }
+    })  */
+ // }
+ let filePath = path.join(__dirname, 'public', req.url === '/' ?
+ 'index.html' : req.url);
+ 
+ //Extension of the file
+
+ let extname = pathe.extname(filePath);
 });
 
 const PORT = process.env.PORT || 5000;
