f45000a8d7cba4e2058e1c0b12c10c3bb374f9f9
salamanderctesiphon <swbrookshire@gmail.com>
2023-07-17 13:52:10 -0500

    following along with the tutorial

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const Person = require('./person');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const person1 = new Person('HOnn Doe', 30);
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+      if(err) throw err;
+      res.writeHead(200, { 'Contnet': 'text/html'});
+      res.end(content);
+    }) 
+  }
+  if(req.url === '/about') {
+    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+      if(err) throw err;
+      res.writeHead(200, { 'Contnet': 'text/html'});
+      res.end(content);
+    }) 
+  }
+});
 
-person1.greeting();
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
