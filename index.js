4c8437baff07fc5c1d533dfb24a37e4a763aa1a2
salamanderctesiphon <swbrookshire@gmail.com>
2023-07-21 11:10:47 -0500

    same old bullshit

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   if(req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html'})
-    res.end()
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+      if(err) throw err;
+      res.writeHead(200, { 'Content-Type': 'text/html'});
+      res.end(content);
+    })
+    
   }
 });
 
